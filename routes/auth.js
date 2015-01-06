900212da12dda0fd222159fe68b9a250b5e750d3
Mark C Allen <mark.allen@hootsuite.com>
2015-01-05 17:09:40 -0800

    Added tests for login and logout auth endpoints.

--- routes/auth.js
+++ routes/auth.js
@@ -39,7 +39,7 @@ module.exports = function(params) {
         ));
 
   passport.use(new BasicStrategy(
-    function(username, password, done) {
+        function(username, password, done) {
           User.findOne({ username: username }, function (err, user) {
             if (err) { 
               return done(err); 
@@ -55,79 +55,79 @@ module.exports = function(params) {
             return done(null, user);
           });
         }
-  ));
+        ));
 
   app.all('/auth/*', function(req, res, next) {
-    res.header("Cache-Control", "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0");
-    next();
-  });
+                  res.header("Cache-Control", "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0");
+                  next();
+                  });
 
-    app.post('/auth/login', function(req, res, next) {
-    var successUrl = (req.body.successUrl != null ? req.body.successUrl : '/#validlogin');
-    var failureUrl = (req.body.failureUrl != null ? req.body.failureUrl : '/#invalidlogin');
-    passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err) }
-    if (!user) { return res.redirect(failureUrl) }
-    req.logIn(user, function(err) {
-    if (err) { return next(err); }
-    return res.redirect(successUrl);
-    });
-    })(req, res, next);
-    });
+                  app.post('/auth/login', function(req, res, next) {
+                  var successUrl = (req.body.successUrl != null ? req.body.successUrl : '/#validlogin');
+                  var failureUrl = (req.body.failureUrl != null ? req.body.failureUrl : '/#invalidlogin');
+                  passport.authenticate('local', function(err, user, info) {
+                  if (err) { return next(err); }
+                  if (!user) { return res.redirect(failureUrl); }
+                  req.logIn(user, function(err) {
+                  if (err) { return next(err); }
+                  return res.redirect(successUrl);
+                  });
+                  })(req, res, next);
+                  });
 
-    app.get('/auth/logout', function(req, res){
-    req.logOut();
-    res.redirect('/');
-    });
+                  app.get('/auth/logout', function(req, res){
+                  req.logOut();
+                  res.redirect('/');
+                  });
 
-    app.post('/auth/forgottenPassword', function (req, res, next){
-    User.find({username: req.body.username}, function(err, user) {
-    if (err) {
-    next(err);
-    } else if (user == null || user.length != 1 || user[0].username != req.body.username ) {
-    next("Can not find user for: " + req.body.username);
-    } else {
-    var buffer = new Array(32);
-    uuid.v4(null, buffer, 0);
-    user[0].recoverSalt = uuid.unparse(buffer);
-    return user[0].save(function (err) {
-    if (err) {
-    next(err); 
-    } else {
-    var url = config.servername + "/#recover/" + user[0].username + "/" + hash(user[0].username, user[0].recoverSalt);
-    var textData = "Use this URL to recover your account: " + url;
-    var htmlData = "Use this URL to recover your account: <a href=\"" + url + "\">" + url + "</a>";
-    var message = {
-    to: user[0].email,
-    from: config.senderaddress,
-    subject: 'Forgotten Password',
-    text: textData,
-    html: htmlData
-    };
-    transport.sendMail(message, function(err){
-    if (err) {
-    return next(err);
-    } else 
-    return res.send("Message sent");
-    });
-    }
-    });
-    }
-    });
-    });
+                  app.post('/auth/forgottenPassword', function (req, res, next){
+                  User.find({username: req.body.username}, function(err, user) {
+                  if (err) {
+                  next(err);
+                  } else if (user == null || user.length != 1 || user[0].username != req.body.username ) {
+                  next("Can not find user for: " + req.body.username);
+                  } else {
+                  var buffer = new Array(32);
+                  uuid.v4(null, buffer, 0);
+                  user[0].recoverSalt = uuid.unparse(buffer);
+                  return user[0].save(function (err) {
+                  if (err) {
+                  next(err); 
+                  } else {
+                  var url = config.servername + "/#recover/" + user[0].username + "/" + hash(user[0].username, user[0].recoverSalt);
+                  var textData = "Use this URL to recover your account: " + url;
+                  var htmlData = "Use this URL to recover your account: <a href=\"" + url + "\">" + url + "</a>";
+                  var message = {
+                  to: user[0].email,
+                  from: config.senderaddress,
+                  subject: 'Forgotten Password',
+                  text: textData,
+                  html: htmlData
+                  };
+                  transport.sendMail(message, function(err){
+                  if (err) {
+                  return next(err);
+                  } else 
+                  return res.send("Message sent");
+                  });
+                  }
+                  });
+                  }
+                  });
+                  });
 
-    app.post('/auth/recoverAccount', function (req, res, next){
-    User.find({username: req.body.username}, function(err, user) {
-    if (err) {
-    next(err);
-    } else if (user == null || user.length != 1 || user[0].username != req.body.username ) {
-    next("Can not find user for: " + req.body.username);
-    } else {
-    if(req.body.hash === hash(req.body.username, user[0].recoverSalt)) {
-    user[0].setPassword(req.body.password);
-    user[0].recoverSalt = "";
-    return user[0].save(function (err) {
-    if (err) {
+                  app.post('/auth/recoverAccount', function (req, res, next){
+                  User.find({username: req.body.username}, function(err, user) {
+                  if (err) {
+                  next(err);
+                  } else if (user == null || user.length != 1 || user[0].username != req.body.username ) {
+                  next("Can not find user for: " + req.body.username);
+                  } else {
+                  if(req.body.hash === hash(req.body.username, user[0].recoverSalt)) {
+                  user[0].setPassword(req.body.password);
+                  user[0].recoverSalt = "";
+                  return user[0].save(function (err) {
+                  if (err) {
     next(err); 
 } else {
   next();
@@ -191,4 +191,4 @@ app.post('/auth/verifyEmail', function (req, res, next) {
   }
 });
 
-}
+};
